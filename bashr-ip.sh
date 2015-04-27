26d78d65cd03e8f29809b01b46ebff7c584edc80
Nicholas Colyer <ncolyer@gmail.com>
2015-04-27 00:28:53 -0400

    Initial Release
    Basic script to configure xen templates. No PCRE on input for
    validation. Add it if you like, I know not to make mistakes on input
    for my VMs. YMMV

---
+++ bashr-ip.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+today=`date +%Y-%m-%d.%H:%M:%S`
+
+echo "Backing up original configurations..."
+cp /etc/sysconfig/network-scripts/ifcfg-$ethernet /etc/sysconfig/network-scripts/ifcfg-$ethernet.$today.bak
+cp /etc/resolv.conf/etc/resolv.conf.$today.bak
+
+# Networking Section
+echo "This auto-provisioning script will allow for the configuration of this new VMs network parameters."
+read -p "Is this network DHCP? " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+        # DHCP
+        echo -n "Enter the device identifer (ex. eth0) and press [ENTER]: "
+        read ethernet
+        dhclient $ethernet
+else
+        # Not DHCP
+        echo -n "Enter the device identifer (ex. eth0) and press [ENTER]: "
+        read ethernet
+        echo -n "Enter the broadcast address (ex. 10.0.2.255) and press [ENTER]: "
+        read broadcast
+        echo -n "Enter the device ip address (ex. 10.0.2.50) and press [ENTER]: "
+        read ipaddress
+        echo -n "Enter the device netmast (ex. 255.255.255.0) and press [ENTER]: "
+        read netmask
+        echo -n "Enter the device network (ex. 10.0.2.0) and press [ENTER]: "
+        read network
+
+        echo "# Generated via auto-provision script by ncolyer@gmail.com" > /etc/sysconfig/network-scripts/ifcfg-$ethernet
+        echo "DEVICE=$ethernet" >> /etc/sysconfig/network-scripts/ifcfg-$ethernet
+        echo "BOOTPROTO=static" >> /etc/sysconfig/network-scripts/ifcfg-$ethernet
+        echo "BROADCAST=$broadcast">> /etc/sysconfig/network-scripts/ifcfg-$ethernet
+        echo "IPADDRESS=$ipaddress">> /etc/sysconfig/network-scripts/ifcfg-$ethernet
+        echo "NETMASK=$netmask">> /etc/sysconfig/network-scripts/ifcfg-$ethernet
+        echo "NETWORK=$network">> /etc/sysconfig/network-scripts/ifcfg-$ethernet
+        echo "ONBOOT=yes">> /etc/sysconfig/network-scripts/ifcfg-$ethernet
+fi
+
+# DNS Section
+echo -n "Enter the search domain (ex. vtech-shared.local) and press [ENTER]: "
+read searchDomain
+echo -n "Enter the primary nameserver (ex. 10.0.2.15) and press [ENTER]: "
+read primaryDns
+echo -n "Enter the secondary nameserver (ex. 10.0.2.16) and press [ENTER]: "
+read secondaryDns
+echo "# Generated via auto-provision script by ncolyer@gmail.com" > /etc/resolv.conf
+echo "search $searchDomain" >> /etc/resolv.conf
+echo "nameserver $primaryDns" >> /etc/resolv.conf
+echo "nameserver $secondaryDns" >> /etc/resolv.conf
+
+# Hostname Settings
+echo -n "Enter the hostname (ex. server01.local) and press [ENTER]: "
+read hostname
+echo "# Generated via auto-provision script by ncolyer@gmail.com" > /etc/sysconfig/network
+echo "NETWORKING=yes" >> /etc/sysconfig/network
+echo "NETWORKING_IPV6=yes" >> /etc/sysconfig/network
+echo "HOSTNAME=$hostname" >> /etc/sysconfig/network
+
+# Restarting services...
+service network restart
+service hostname restart
+
+echo "Script completed."
