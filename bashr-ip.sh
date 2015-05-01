25c75dd174d0b301eb5dfd7e56a15555dd0bd833
Nicholas Colyer <ncolyer@gmail.com>
2015-04-30 22:37:32 -0400

    Removed deprecated func and change order of a few lines

--- bashr-ip.sh
+++ bashr-ip.sh
@@ -32,14 +32,15 @@ then
 	# DHCP
 	echo -ne "\nEnter the device identifer (ex. eth0) and press [ENTER]: "
 	read ethernet
-	cp /etc/sysconfig/network-scripts/ifcfg-$ethernet /etc/sysconfig/network-scripts/ifcfg-$ethernet.$today.bak
+	if [ -z "$ethernet" ]; then
+		ethernet="eth0"
+	fi
 	dhclient $ethernet
+	cp /etc/sysconfig/network-scripts/ifcfg-$ethernet /etc/sysconfig/network-scripts/ifcfg-$ethernet.$today.bak
 else
 	# Not DHCP
 	echo -ne "\nEnter the device identifer (ex. eth0) and press [ENTER]: "
 	read ethernet
-	echo -n "Enter the broadcast address (ex. 10.0.2.255) and press [ENTER]: "
-	read broadcast
 	echo -n "Enter the device ip address (ex. 10.0.2.50) and press [ENTER]: "
 	read ipaddress
 	echo -n "Enter the device netmast (ex. 255.255.255.0) and press [ENTER]: "
@@ -47,7 +48,7 @@ else
 	echo -n "Enter the device network (ex. 10.0.2.0) and press [ENTER]: "
 	read network
 
-	if [ -z "$broadcast" ] || [ -z "$ipaddress"] || ["$netmask"]; then	
+	if [ -z "$ipaddress"] || ["$netmask"]; then	
 		echo -ne "\n\n\n\n\033[0;31mMissing required parameters. Please run the setup.sh shell script again to re-start provisioning.\033[0m"
 		echo -ne "\n\n\nExiting...\n\n"
 		sleep 2
@@ -62,25 +63,24 @@ else
 		echo "DEVICE=$ethernet" >> /etc/sysconfig/network-scripts/ifcfg-$ethernet
 	fi
 	echo "BOOTPROTO=static" >> /etc/sysconfig/network-scripts/ifcfg-$ethernet
-	echo "BROADCAST=$broadcast">> /etc/sysconfig/network-scripts/ifcfg-$ethernet
 	echo "IPADDRESS=$ipaddress">> /etc/sysconfig/network-scripts/ifcfg-$ethernet
 	echo "NETMASK=$netmask">> /etc/sysconfig/network-scripts/ifcfg-$ethernet
 	echo "NETWORK=$network">> /etc/sysconfig/network-scripts/ifcfg-$ethernet
 	echo "ONBOOT=yes">> /etc/sysconfig/network-scripts/ifcfg-$ethernet
-fi
 
-# DNS Section
-echo -n "Enter the search domain (ex. yourdomain.local) and press [ENTER]: "
-read searchDomain
-echo -n "Enter the primary nameserver (ex. 10.0.2.15) and press [ENTER]: "
-read primaryDns
-echo -n "Enter the secondary nameserver (ex. 10.0.2.16) and press [ENTER]: "
-read secondaryDns
-cp /etc/resolv.conf /etc/resolv.conf.$today.bak
-echo "# Generated via auto-provision script by ncolyer@gmail.com" > /etc/resolv.conf
-echo "search $searchDomain" >> /etc/resolv.conf
-echo "nameserver $primaryDns" >> /etc/resolv.conf
-echo "nameserver $secondaryDns" >> /etc/resolv.conf
+	# DNS Section
+	echo -n "Enter the search domain (ex. yourdomain.local) and press [ENTER]: "
+	read searchDomain
+	echo -n "Enter the primary nameserver (ex. 10.0.2.15) and press [ENTER]: "
+	read primaryDns
+	echo -n "Enter the secondary nameserver (ex. 10.0.2.16) and press [ENTER]: "
+	read secondaryDns
+	cp /etc/resolv.conf /etc/resolv.conf.$today.bak
+	echo "# Generated via auto-provision script by ncolyer@gmail.com" > /etc/resolv.conf
+	echo "search $searchDomain" >> /etc/resolv.conf
+	echo "nameserver $primaryDns" >> /etc/resolv.conf
+	echo "nameserver $secondaryDns" >> /etc/resolv.conf
+fi
 
 # Hostname Settings
 echo -n "Enter the hostname (ex. server01.local) and press [ENTER]: "
@@ -91,5 +91,4 @@ echo "NETWORKING_IPV6=yes" >> /etc/sysconfig/network
 echo "HOSTNAME=$hostname" >> /etc/sysconfig/network
 
 echo -ne "\nScript completed. Rebooting..."
-reboot
-
+shutdown -r 10
